fa41bd39a3b969dde8a721e7c537fc43eaff5e42
kouassij <jacobcn21@gmail.com>
2023-11-06 15:53:41 +0100

    first enregistrement

---
+++ JAVASCRIPT/global.js
@@ -0,0 +1,8 @@
+function color() {
+  document.getElementById("sauter").style.background = "#900C3F";
+  document.getElementById("sauter").style.color = "white";
+}
+function color2() {
+  document.getElementById("start").style.background = "#25CB4B";
+  document.getElementById("start").style.color = "white";
+}
